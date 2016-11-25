f357a800c7b7895bb6c9f0028b531b9582b71321
Tavleen Singh <tavleensingh@Tavleens-MacBook-Air.local>
2016-11-24 16:09:18 -0800

    added timer to the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,54 @@
 $(document).ready(function(){
-    $("#startButton").click(function(){
-        $("#quizSection").show();
-        $("#startButton").hide();
-    });
+
+	//var totalNumberOfQuestions = 10;
+	var correctAnswer = 0;
+	var incorrectAnswer = 0;
+	var totalTime = 10;
+	var counter ;
+	var unansweredQuestions = 0;
+
+
+	function start(){
+		counter = setInterval(decrement, 1000);
+		$("#quizSection").show();
+		$("#startButton").hide();
+	};
+
+	function submit(){
+		clearInterval(counter);
+	};
+
+	function decrement() {
+    //  Decrease number by one.
+      totalTime--;
+      //  Show the number in the #show-number tag.
+      $("#timeDisplay").html("<h2>" + "Time Remaining: " + totalTime + "</h2>");
+      //  Once number hits zero...
+      if (totalTime === 0) {
+        //  ...run the stop function.
+        submit();
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+
+    $(document).on("click", "#startButton", function(){
+		start();
+	});
+
+	$(document).on("click", "#submitButton", function(){
+		submit();
+	});
+
+
+
+
+
+
+
+
+
+
+
 });
